82b75a4280998b64d28a74572303876a39e500a5
Ahmed <atomcgarry@gmail.com>
2022-07-16 10:35:27 +0100

    _

--- src/jobs/jobs.routes.ts
+++ src/jobs/jobs.routes.ts
@@ -3,8 +3,8 @@ import { getAdzunaJobs, getReedJob, getReedJobs } from "./jobs.controller"
 
 const jobsRouter = Router()
 
-jobsRouter.get("api/reed", getReedJobs)
-jobsRouter.get("api/adzuna", getAdzunaJobs)
-jobsRouter.get("api/reed/:id", getReedJob)
+jobsRouter.get("/api/reed", getReedJobs)
+jobsRouter.get("/api/adzuna", getAdzunaJobs)
+jobsRouter.get("/api/reed/:id", getReedJob)
 
 export default jobsRouter
--- src/user/user.routes.ts
+++ src/user/user.routes.ts
@@ -3,10 +3,10 @@ import { addUser, login, getUsers, deleteUser, editUser } from "./user.controlle
 
 const userRouter = Router()
 
-userRouter.post("api/user", addUser) // DONE JUST NEED TO LOG IN NOW
-userRouter.post("api/login", login) // DONE
-userRouter.get("api/user", getUsers)
-userRouter.put("api/user", editUser)
-userRouter.delete("api/user/:id", deleteUser) // EVERY USER NEEDS AN ID
+userRouter.post("/api/user", addUser) // DONE JUST NEED TO LOG IN NOW
+userRouter.post("/api/login", login) // DONE
+userRouter.get("/api/user", getUsers)
+userRouter.put("/api/user", editUser)
+userRouter.delete("/api/user/:id", deleteUser) // EVERY USER NEEDS AN ID
 
 export default userRouter
