4a20bf7d76089c94dff3e91067471687db0994e6
Ahmed <atomcgarry@gmail.com>
2022-08-08 19:22:04 +0100

    undo google

--- src/user/user.google.controller.ts
+++ src/user/user.google.controller.ts
@@ -1,92 +1,92 @@
-/* eslint-disable indent */
-/* eslint-disable no-empty */
-import { NextFunction, Request, Response } from "express"
-import db from "../../src/db/connection"
-import { ROW_TYPE, UserStateInterface } from "./user.model"
+// /* eslint-disable indent */
+// /* eslint-disable no-empty */
+// import { NextFunction, Request, Response } from "express"
+// import db from "../../src/db/connection"
+// import { ROW_TYPE, UserStateInterface } from "./user.model"
 
-const googleLogin = async (req: Request, res: Response, next: NextFunction) => {
-  // Check if this user exists
-  // IF exists login to his account
-  // return state
-  try {
-    const sql_find = "SELECT * FROM users WHERE email = (?)"
-    const { email } = req.body
+// const googleLogin = async (req: Request, res: Response, next: NextFunction) => {
+//   // Check if this user exists
+//   // IF exists login to his account
+//   // return state
+//   try {
+//     const sql_find = "SELECT * FROM users WHERE email = (?)"
+//     const { email } = req.body
 
-    db.all(sql_find, [email], (err: any, rows: Array<ROW_TYPE>) => {
-      if (err) return console.error(err)
-      if (rows.length === 1) {
-        const {
-          first_name,
-          last_name,
-          email,
-          liked,
-          applied,
-          interview,
-          accepted,
-          rejected,
-        } = rows[0]
+//     db.all(sql_find, [email], (err: any, rows: Array<ROW_TYPE>) => {
+//       if (err) return console.error(err)
+//       if (rows.length === 1) {
+//         const {
+//           first_name,
+//           last_name,
+//           email,
+//           liked,
+//           applied,
+//           interview,
+//           accepted,
+//           rejected,
+//         } = rows[0]
 
-        const state: UserStateInterface = {
-          isLoggedIn: true,
-          user: {
-            firstName: first_name,
-            lastName: last_name,
-            email: email,
-          },
-          savedJobs: {
-            likedJobs: liked,
-            appliedJobs: applied,
-            interviewJobs: interview,
-            acceptedJobs: accepted,
-            rejectedJobs: rejected,
-          },
-        }
+//         const state: UserStateInterface = {
+//           isLoggedIn: true,
+//           user: {
+//             firstName: first_name,
+//             lastName: last_name,
+//             email: email,
+//           },
+//           savedJobs: {
+//             likedJobs: liked,
+//             appliedJobs: applied,
+//             interviewJobs: interview,
+//             acceptedJobs: accepted,
+//             rejectedJobs: rejected,
+//           },
+//         }
 
-        res.status(200).send({ message: "Success", user: state })
-      }
+//         res.status(200).send({ message: "Success", user: state })
+//       }
 
-      if (rows.length === 0) {
-          next()
-      }
-    })
-  } catch (error) {}
-}
+//       if (rows.length === 0) {
+//           next()
+//       }
+//     })
+//   } catch (error) {}
+// }
 
-const googleSignUp = async (res: Response, req: Request) => {
-    // IF does not exist, create account for the user
-    // LEAVE password field blank
-    // return state
-    try {
-        const sql_insert = "INSERT INTO users(first_name, last_name, email, password) VALUES(?,?,?,?)"
-		const { first_name, last_name, email } =  req.body 
+// const googleSignUp = async (res: Response, req: Request) => {
+//     // IF does not exist, create account for the user
+//     // LEAVE password field blank
+//     // return state
+//     try {
+//         const sql_insert = "INSERT INTO users(first_name, last_name, email, password) VALUES(?,?,?,?)"
+// 		const { first_name, last_name, email } =  req.body 
 
-		db.run(sql_insert,[first_name, last_name, email, ""], (err: any) => {
-			if (err) return console.error(err)
-			console.log("a new row has been created")
-		})
+// 		db.run(sql_insert,[first_name, last_name, email, ""], (err: any) => {
+// 			if (err) return console.error(err)
+// 			console.log("a new row has been created")
+// 		})
 
-        const state: UserStateInterface = {
-            isLoggedIn: true,
-            user: {
-              firstName: first_name,
-              lastName: last_name,
-              email: email,
-            },
-            savedJobs: {
-              likedJobs: [],
-              appliedJobs: [],
-              interviewJobs: [],
-              acceptedJobs: [],
-              rejectedJobs: [],
-            },
-          }
+//         const state: UserStateInterface = {
+//             isLoggedIn: true,
+//             user: {
+//               firstName: first_name,
+//               lastName: last_name,
+//               email: email,
+//             },
+//             savedJobs: {
+//               likedJobs: [],
+//               appliedJobs: [],
+//               interviewJobs: [],
+//               acceptedJobs: [],
+//               rejectedJobs: [],
+//             },
+//           }
 
-		res
-			.status(200)
-			.send({ message: "Success", user: state })
-    } catch (error) {
-        console.log(error)
-    }
-}
+// 		res
+// 			.status(200)
+// 			.send({ message: "Success", user: state })
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
 
-export { googleLogin, googleSignUp }
+// export { googleLogin, googleSignUp }
--- src/user/user.routes.ts
+++ src/user/user.routes.ts
@@ -1,12 +1,12 @@
 import { Router } from "express"
 import { addUser, login, getUsers, deleteUser, editUser } from "./user.controller"
-import { googleLogin, googleSignUp } from "./user.google.controller"
+// import { googleLogin, googleSignUp } from "./user.google.controller"
 
 const userRouter = Router()
 
 userRouter.post("/api/user", addUser, login) // DONE JUST NEED TO LOG IN NOW
 userRouter.post("/api/login", login) // DONE
-userRouter.post("/api/google-auth", googleLogin, googleSignUp)
+// userRouter.post("/api/google-auth", googleLogin, googleSignUp)
 userRouter.get("/api/user", getUsers)
 userRouter.put("/api/user", editUser)
 userRouter.delete("/api/user/:id", deleteUser) // EVERY USER NEEDS AN ID
