795287f7e41ea96ddd626a66fc8ae58fc47ea77b
Ahmed <atomcgarry@gmail.com>
2022-08-09 19:59:44 +0100

    +

--- src/server.ts
+++ src/server.ts
@@ -15,6 +15,13 @@ app
 	.use(userRouter)
 	.use(jobsRouter)
 
+app.all("*", function (req, res) {
+	res.header("Access-Control-Allow-Origin", "*")
+	res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With")
+	res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS")
+	//...
+})
+
 app.get("/", (_, res: Response) => {
 	res
 		.status(200)
