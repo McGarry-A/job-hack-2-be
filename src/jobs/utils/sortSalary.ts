c2cc557e20d49d8b9309b55456da621ccabd7910
Ahmed McGarry <atomcgarry@gmail.com>
2022-09-01 15:56:50 +0100

    _

--- src/jobs/utils/sortSalary.ts
+++ src/jobs/utils/sortSalary.ts
@@ -2,13 +2,24 @@ import { JobInterface } from "../jobs.model"
 
 const sortSalary = (sortBy: "ascending" | "descending", jobsArray: JobInterface[]) => {
 	console.log("sorting by salary")
+
+	if (sortBy === "ascending") {
+		const sortedArray = jobsArray.sort((a, b) => {
+			if (a.salary > b.salary) return -1
+			if (a.salary < b.salary) return 1
+			return 0
+		})
+		return sortedArray
+	}
+    
 	const sortedArray = jobsArray.sort((a, b) => {
-		if (a.salary > b.salary) return -1
-		if (a.salary < b.salary) return 1
+		if (a.salary > b.salary) return 1
+		if (a.salary < b.salary) return -1
 		return 0
 	})
 
 	return sortedArray
+
 }
 
 export default sortSalary
